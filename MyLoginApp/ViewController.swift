59bc3d5feedaa0503c3a6b1227c2db02b6de31da
Artem Karmaz <inste@icloud.com>
2019-01-24 13:35:29 +0200

    first commit

--- MyLoginApp/ViewController.swift
+++ MyLoginApp/ViewController.swift
@@ -12,9 +12,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: nil)
+        {nc in self.view.frame.origin.y = +200}
+        
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: nil)
+        {nc in self.view.frame.origin.y = 0}
+        
+        userTextField[0].delegate = self
+        userTextField[1].delegate = self
     }
 
-
+    @IBOutlet var userTextField: [UITextField]!
+    
+    @IBAction func enterHomeButton(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    @IBAction func registerAccountButton(_ sender: UIButton) {
+        
+        
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userTextField[0] || textField == userTextField[1] {
+            self.userTextField[0].resignFirstResponder()
+            self.userTextField[1].resignFirstResponder()
+        }
+        return true
+    }
+}
+
+extension String {
+    func matchec(_ regex: String) -> Bool {
+        return self.range(of: regex, options: .regularExpression, range: nil, locale: nil) != nil
+    }
+}
