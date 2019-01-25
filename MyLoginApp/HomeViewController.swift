2c701bfd48b301aaaef56ddba378b8e2e5fd44a1
Artem Karmaz <inste@icloud.com>
2019-01-25 12:56:42 +0200

    fourth commit

---
+++ MyLoginApp/HomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewController.swift
+//  MyLoginApp
+//
+//  Created by Artem Karmaz on 1/25/19.
+//  Copyright © 2019 Johansson Group. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func backButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
