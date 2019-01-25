234268b59eb03826e3306147b5d2ccfe03fffd66
Artem Karmaz <inste@icloud.com>
2019-01-25 14:16:11 +0200

    eight commit

--- MyLoginApp/Data.swift
+++ MyLoginApp/Data.swift
@@ -33,11 +33,33 @@ struct Data {
     }
     
     mutating func registerAccount(_ emailData: String, _ passwordData: String) {
-        dataStorage.updateValue(emailData, forKey: passwordData)
+        dataStorage.updateValue(passwordData, forKey: emailData)
         
         for i in dataStorage {
             print("Login = \(i.key), password = \(i.value)")
         }
+    }
+    
+    func emailAddressCheck(_ emailAddress: String) -> Bool {
+        
+        var returnValue = true
+        let emailRegEx = "[A-Z0-9a-z.-_]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,3}"
+        
+        do {
+            let regularExpression = try NSRegularExpression(pattern: emailRegEx)
+            let nsString = emailAddress as NSString
+            let results = regularExpression.matches(in: emailAddress, range: NSRange(location: 0, length: nsString.length))
+            
+            if results.count == 0 {
+                returnValue = false
+            }
+            
+        } catch let error as NSError {
+            print("invalid regular expression: \(error.localizedDescription)")
+            returnValue = false
+        }
+        
+        return  returnValue
     }
 }
 
@@ -46,10 +68,3 @@ extension String {
         return self.range(of: regex, options: .regularExpression, range: nil, locale: nil) != nil
     }
 }
-
-//
-//func textFieldDidEndEditing(_ textField: UITextField) {
-//
-//    //        let b = String.matches("^[a-zA-Z0-9._-]{1,30}$")
-//    //        print(b)
-//}
--- MyLoginApp/RegisterViewController.swift
+++ MyLoginApp/RegisterViewController.swift
@@ -12,20 +12,42 @@ class RegisterViewController: UIViewController {
     
     var data = Data()
     
+    func displayAlertMessage(messageToDisplay: String)
+    {
+        let alertController = UIAlertController(title: "Alert", message: messageToDisplay, preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
+            // Code in this block will trigger when OK button tapped.
+            print("Ok button tapped");
+        }
+        alertController.addAction(OKAction)
+        self.present(alertController, animated: true, completion:nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         userTextField[0].delegate = self
         userTextField[1].delegate = self
+        userTextField[2].delegate = self
     }
     
     @IBOutlet var userTextField: [UITextField]!
     
     @IBAction func registerButton(_ sender: UIButton) {
         
-        _ = data.registerAccount(userTextField[0].text!, userTextField[1].text!)
+//        let providedEmailAddress = userTextField[0].text
+        let isEmailAddressValid = data.emailAddressCheck(userTextField[0].text!)
         
+        if isEmailAddressValid {
+            print("Email ok!")
+        } else {
+            print("Email not ok")
+            displayAlertMessage(messageToDisplay: "Email address is not valid")
+        }
+        
+        _ = data.registerAccount(userTextField[0].text!, userTextField[1].text!)
     }
+    
     @IBAction func backButton(_ sender: UIButton) {
         let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let loginViewController = storyboard.instantiateViewController(withIdentifier: "loginVC") as! LoginViewController
@@ -38,9 +60,10 @@ class RegisterViewController: UIViewController {
 
 extension RegisterViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField == userTextField[0] || textField == userTextField[1] {
+        if textField == userTextField[0] || textField == userTextField[1] || textField == userTextField[2] {
             self.userTextField[0].resignFirstResponder()
             self.userTextField[1].resignFirstResponder()
+            self.userTextField[2].resignFirstResponder()
         }
         return true
     }
